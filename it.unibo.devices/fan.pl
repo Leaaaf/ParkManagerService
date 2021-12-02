50b976578a192568be9a6e4faa02224741a987a6
Leaaaf <alessandro.foglia@outlook.it>
2021-12-02 19:16:07 +0100

    parkingmanagerservice

--- it.unibo.devices/fan.pl
+++ it.unibo.devices/fan.pl
@@ -2,4 +2,4 @@
 % fan description   
 %====================================================================================
 context(ctxfan, "localhost",  "TCP", "8001").
- qactor( fan, ctxfan, "it.unibo.fan.Fan").
+ qactor( fanactor, ctxfan, "it.unibo.fanactor.Fanactor").
---
+++ it.unibo.parkmanagerservice/carparking.pl
@@ -0,0 +1,14 @@
+%====================================================================================
+% carparking description   
+%====================================================================================
+context(ctxcarparking, "localhost",  "TCP", "8000").
+context(ctxfan, "localhost",  "TCP", "8001").
+context(ctxweightsensor, "localhost",  "TCP", "8002").
+context(ctxthermometer, "localhost",  "TCP", "8003").
+context(ctxoutsonarsensor, "localhost",  "TCP", "8004").
+context(ctxbasicrobot, "localhost",  "TCP", "8020").
+ qactor( fanactor, ctxfan, "external").
+  qactor( weightsensoractor, ctxweightsensor, "external").
+  qactor( thermometeractor, ctxthermometer, "external").
+  qactor( outsonaractor, ctxoutsonarsensor, "external").
+  qactor( parkingmanagerservice, ctxcarparking, "it.unibo.parkingmanagerservice.Parkingmanagerservice").
