c7568f9be3b51695793abd493e0299f988ebb727
Leaaaf <alessandro.foglia@outlook.it>
2022-06-18 11:37:18 +0200

    fixed login

--- it.unibo.webapp/src/manager/manager.controller.ts
+++ it.unibo.webapp/src/manager/manager.controller.ts
@@ -1,20 +1,27 @@
 import { Controller, Get, Render, Req, Res, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { ManagerService } from './manager.service';
 
 @ApiTags("manager")
 @Controller('manager')
 export class ManagerController {
   constructor(
-    private readonly managerService: ManagerService
+    private readonly jwtService: JwtService
   ) {}
 
   @Get()
   async handleManagerLogin(@Req() req, @Res({ passthrough: true }) res: Response) {
-    return req.cookies["X-AUTH-TOKEN"] != undefined ? 
-      res.redirect("manager/console") : res.redirect("manager/login");
+    if (req.cookies["X-AUTH-TOKEN"] != undefined) {
+      const decoded_token = this.jwtService.decode(req.cookies["X-AUTH-TOKEN"]);
+      let now: string | number = Date.now().toString()
+      now = Number(now.substring(0, now.length - 3))
+
+      if (decoded_token["exp"] - now > 0)
+        return res.redirect("manager/console")
+    }
+    return res.redirect("manager/login");
   }
 
   @Get("console")
@@ -26,6 +33,5 @@ export class ManagerController {
   @Get("login")
   @Render("manager/login")
   login() {
-    
   }
 }
--- it.unibo.webapp/src/manager/manager.module.ts
+++ it.unibo.webapp/src/manager/manager.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ManagerService } from './manager.service';
 import { ManagerController } from './manager.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [ManagerController],
   providers: [ManagerService]
 })
