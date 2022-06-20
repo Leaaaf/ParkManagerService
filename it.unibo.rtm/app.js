b66edf40d5d3f5bf998782276df55a1f19ef9cd5
Leaaaf <alessandro.foglia@outlook.it>
2022-06-20 08:02:28 +0200

    rtm refactoring

--- it.unibo.rtm/app.js
+++ it.unibo.rtm/app.js
@@ -7,6 +7,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// COAP 
+const coap = require('coap')
+const coapServer = coap.createServer()
+
 app.get("/", function (req, res) {
   res.send("hello world");
 });
@@ -26,3 +30,27 @@ io.on("connection", (socket) => {
 
 //module.exports = app
 server.listen(8000)
+
+coapServer.listen(() => {
+  console.log("RTM | Coap Server listening...")
+  const req = coap.request('coap://localhost:8001/ctxthermometer/thermometeractor')
+  req.on('response', (res) => {
+    res.pipe(process.stdout)
+    res.on('end', () => {
+      process.exit(0)
+    })
+  })
+
+  req.end();
+})
+
+
+
+/*
+{"resource": "thermometer","coapUrl": "coap://localhost:8001/ctxthermometer/thermometeractor","fwport": "0","fwwsurl":"ws://localhost:8000/manager/thermometer/","fwprotocol": "ws", "proxytype": "coroutined"}
+{"resource": "fan","coapUrl": "coap://localhost:8002/ctxfan/fanactor","fwport": "0","fwwsurl":"ws://localhost:8000/manager/fan/","fwprotocol": "ws", "proxytype": "coroutined"}
+{"resource": "sonar","coapUrl": "coap://localhost:8003/ctxsonar/sonaractor","fwport": "0","fwwsurl":"ws://localhost:8000/manager/sonar/","fwprotocol": "ws", "proxytype": "coroutined"}
+{"resource": "weightsensor","coapUrl": "coap://localhost:8004/ctxweightsensor/weightsensoractor","fwport": "0","fwwsurl":"ws://localhost:8000/manager/weightsensor/","fwprotocol": "ws", "proxytype": "coroutined"}
+{"resource": "parkingmanager","coapUrl": "coap://localhost:8010/ctxcarparking/parkingmanagerservice","fwport": "0","fwwsurl":"ws://localhost:8000/manager/carparking/","fwprotocol": "ws", "proxytype": "coroutined"}
+{"resource": "trolley","coapUrl": "coap://localhost:8010/ctxcarparking/trolley","fwport": "0","fwwsurl":"ws://localhost:8000/manager/trolley/","fwprotocol": "ws", "proxytype": "coroutined"} 
+*/
